3772510c53fa9fafdaa59184c85c35a8e406b960
Spiridonov-ISU <56498495+Spiridonov-ISU@users.noreply.github.com>
2021-01-22 22:51:40 +0800

    Add files via upload

---
+++ SinglyLinkedList/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include "List.h"
+
+using namespace std;
+
+int main()
+{
+	List<int> lst;
+	lst.pushFront(5);
+	lst.pushFront(7);
+	lst.pushFront(101);
+
+	for (int i = 0; i < lst.getSize(); i++)
+	{
+		cout << lst[i] << endl;
+	}
+
+	cout << endl << "popBack " << endl << endl;
+
+	lst.popBack();
+
+	for (int i = 0; i < lst.getSize(); i++)
+	{
+		cout << lst[i] << endl;
+	}
+
+	return 0;
+}
