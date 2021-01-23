17d7f7ddf8b1334f3cc151956bbaa641576fb182
Spiridonov-ISU <56498495+Spiridonov-ISU@users.noreply.github.com>
2021-01-23 20:49:07 +0800

    Add files via upload

---
+++ Rational/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "rational.h"
+
+using namespace std;
+
+int main()
+{
+    Rational a(4, 1), b(1, 6);
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << "\n\n";
+
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "a / b = " << a / b << "\n\n";
+ 
+    cout << "(a > b) = " << (a > b) << endl;
+    cout << "(a < b) = " << (a < b) << endl;
+    cout << "(a >= b) = " << (a >= b) << endl;
+    cout << "(a <= b) = " << (a <= b) << endl;
+    cout << "(a == b) = " << (a == b) << endl;
+    cout << "(a != b) = " << (a != b) << "\n\n";
+
+    cout << "a.Rational_sqrt = " << a.Rational_sqrt() << endl;
+}
---
+++ Rational/rational.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+using namespace std;
+
+class Rational {
+private:
+    int numer;
+    int denom;
+    void simplify();
+public:
+    Rational();
+    Rational(int n);
+    Rational(int n, int d);
+
+    Rational& operator += (const Rational& r);
+    Rational operator + (const Rational& r) const;
+    Rational operator -() const;
+    Rational& operator -= (const Rational& r);
+    Rational operator - (const Rational& r) const;
+    Rational& operator ++();
+    Rational operator ++(int);
+
+    bool operator == (const Rational& r) const;
+    bool operator != (const Rational& r) const;
+
+    operator int() const;
+    operator double() const;
+
+    friend istream& operator >> (istream& in, Rational& r);
+    friend ostream& operator << (ostream& out, const Rational& r);
+
+    //
+
+    Rational& operator *= (const Rational& r);
+    Rational operator * (const Rational& r) const;
+    Rational& operator /= (const Rational& r);
+    Rational operator / (const Rational& r) const;
+
+    Rational& operator --();
+    Rational operator --(int);
+
+    bool operator > (const Rational& r) const;
+    bool operator < (const Rational& r) const;
+    bool operator >= (const Rational& r) const;
+    bool operator <= (const Rational& r) const;
+
+    Rational Rational_sqrt();
+};
